d3e838dc890013171f00a587b496e1a668ca68db
Isaikin Roman <romix.lab@gmail.com>
2015-07-15 21:18:17 +0300

    Doc work

--- doc/source/index.rst
+++ doc/source/index.rst
@@ -1,10 +1,7 @@
-.. qcoap documentation master file, created by
-   sphinx-quickstart on Tue Jul 14 18:50:10 2015.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to qcoap's documentation!
 =================================
+
+Insert somewhere: node, Coap node, endpoint, CoAP endpoint are equivalents in this documentation
 
 Contents:
 
--- src/lib/coapexchangeparameters.h
+++ src/lib/coapexchangeparameters.h
@@ -2,6 +2,7 @@
 #define COAPEXCHANGEPARAMETERS_H
 
 #include "coaplib_global.h"
+#include <QJsonObject>
 
 class COAPLIB_SHARED_EXPORT CoapExchangeParameters
 {
@@ -21,25 +22,60 @@ public:
      * +-------------------+---------------+
      */
     CoapExchangeParameters();
-
+    /**
+     * @brief unpack unpacks data stored in json object
+     * @param packed
+     * Used by @see CoapProvision
+     */
+    void unpack(const QJsonObject &packed);
+    /**
+     * @brief pack packs all the data to json object
+     * @return
+     * Used by @see CoapProvision
+     */
+    QJsonObject pack() const;
+    /**
+     * @brief setAckTimeout
+     * @param ackTimeout
+     * Delay before next retransmission:
+     * delay = ackTimeout() * 2 ^ i;
+     * delay += norm(delay * jitter());
+     * where i is is the retransmission number
+     */
     void setAckTimeout(quint16 ackTimeout);
-
+    /**
+     * @brief ackTimeout
+     * @return
+     */
     quint16 ackTimeout() const;
-
-    void setRandomFactor(double randomFactor);
-
-    double randomFactor() const;
-
+    /**
+     * @brief setJitter
+     * @param jitter
+     * @see ackTimeout()
+     */
+    void setJitter(double jitter);
+    /**
+     * @brief jitter
+     * @return jitter value
+     */
+    double jitter() const;
+    /**
+     * @brief setMaxRetransmit setts retransmission events count before giving up on exchange
+     * @param maxRetransmit
+     */
     void setMaxRetransmit(quint8 maxRetransmit);
-
+    /**
+     * @brief maxRetransmit
+     * @return retransmission events count before giving up on exchange
+     */
     quint8 maxRetransmit() const;
-
+    /**
+     * @brief setNStart
+     * @param NStart number of simultaneous outstanding interactions to some node
+     */
     void setNStart(quint8 NStart);
-
     quint8 nSTart() const;
-
     void setProbindRate(quint32 probingRate);
-
     quint32 probingRate() const;
 };
 
